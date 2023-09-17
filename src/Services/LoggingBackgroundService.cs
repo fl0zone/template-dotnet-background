a099d28c55f9fa3de72a8df883386d5af0d97bad
Peter O'Brien <peter@fl0.com>
2023-09-18 07:57:02 +1000

    background service

---
+++ src/Services/LoggingBackgroundService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace Example.API.Services;
+
+public class LoggingBackgroundService : BackgroundService
+{
+    private readonly PeriodicTimer _timer;
+
+    public LoggingBackgroundService()
+    {
+        _timer = new PeriodicTimer(TimeSpan.FromMinutes(1));
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (await _timer.WaitForNextTickAsync(stoppingToken) && stoppingToken.IsCancellationRequested is false)
+        {
+            Log.Information("Background log message");
+        }
+    }
+}
